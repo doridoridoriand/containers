dd555a81e493ad8a0f92326d20e706fe9efa7acc
doridoridoriand <i.am.eager.for.peace@gmail.com>
2020-09-05 09:42:28 +0900

    DockerHubとGitHubのpackageに両方push出来るようにした

--- lifeboat/centos/release.sh
+++ lifeboat/centos/release.sh
@@ -4,6 +4,16 @@ unixtime=`date +%s`;
 
 docker tag `docker images centos-lifeboat:latest --format "{{.ID}}"` doridoridoriand/lifeboat:centos-latest;
 docker tag `docker images centos-lifeboat:latest --format "{{.ID}}"` doridoridoriand/lifeboat:centos-$unixtime;
+
 docker login;
+
 docker push doridoridoriand/lifeboat:centos-latest;
 docker push doridoridoriand/lifeboat:centos-$unixtime;
+
+docker tag `docker images centos-lifeboat:latest --format "{{.ID}}"` docker.pkg.github.com/doridoridoriand/containers/lifeboat:centos-latest;
+docker tag `docker images centos-lifeboat:latest --format "{{.ID}}"` docker.pkg.github.com/doridoridoriand/containers/lifeboat:centos-$unixtime;
+
+cat ~/GH_TOKEN.txt | docker login docker.pkg.github.com -u doridoridoriand --password-stdin;
+
+docker push docker.pkg.github.com/doridoridoriand/containers/lifeboat:centos-latest;
+docker push docker.pkg.github.com/doridoridoriand/containers/lifeboat:centos-$unixtime;
