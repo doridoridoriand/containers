8bc1b52d758045bd8ddccc51b878132d694f6b91
doridoridoriand <i.am.eager.for.peace@gmail.com>
2023-05-27 15:13:27 +0900

    Tweak

--- lifeboat/ubuntu/release.sh
+++ lifeboat/ubuntu/release.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+docker_ps_result=`docker ps 2>&1 > /dev/null`;
+
+if [[ $docker_ps_result == *running?* ]]; then
+  echo "ERROR: docker engine not running. Build failed." >&2;
+  exit 1;
+fi
+
 unixtime=`date +%s`;
 
 docker tag `docker images ubuntu-lifeboat:latest --format "{{.ID}}"` doridoridoriand/lifeboat:ubuntu-latest;
 docker tag `docker images ubuntu-lifeboat:latest --format "{{.ID}}"` doridoridoriand/lifeboat:ubuntu-$unixtime;
+
 docker login;
+
 docker push doridoridoriand/lifeboat:ubuntu-latest;
 docker push doridoridoriand/lifeboat:ubuntu-$unixtime;
+
+docker tag `docker images ubuntu-lifeboat:latest --format "{{.ID}}"` docker.pkg.github.com/doridoridoriand/containers/lifeboat:ubuntu-latest;
+docker tag `docker images ubuntu-lifeboat:latest --format "{{.ID}}"` docker.pkg.github.com/doridoridoriand/containers/lifeboat:ubuntu-$unixtime;
+
+cat ~/GH_TOKEN.txt | docker login docker.pkg.github.com -u doridoridoriand --password-stdin;
+
+docker push docker.pkg.github.com/doridoridoriand/containers/lifeboat:ubuntu-latest;
+docker push docker.pkg.github.com/doridoridoriand/containers/lifeboat:ubuntu-$unixtime;
