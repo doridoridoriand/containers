f26d9ecfe751d0a8ef206d0cd2052cc054ea7323
doridoridoriand <i.am.eager.for.peace@gmail.com>
2023-07-29 20:14:25 +0900

    Add more 2 previous versions.

--- lifeboat/ubuntu/release.sh
+++ lifeboat/ubuntu/release.sh
@@ -8,19 +8,54 @@ if [[ $docker_ps_result == *running?* ]]; then
 fi
 
 unixtime=`date +%s`;
-
-docker tag `docker images ubuntu-lifeboat:latest --format "{{.ID}}"` doridoridoriand/lifeboat-ubuntu:latest;
-docker tag `docker images ubuntu-lifeboat:latest --format "{{.ID}}"` doridoridoriand/lifeboat-ubuntu:$unixtime;
-
 docker login;
-
-docker push doridoridoriand/lifeboat-ubuntu:latest;
-docker push doridoridoriand/lifeboat-ubuntu:$unixtime;
-
-docker tag `docker images ubuntu-lifeboat:latest --format "{{.ID}}"` ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:latest;
-docker tag `docker images ubuntu-lifeboat:latest --format "{{.ID}}"` ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:$unixtime;
-
 cat ~/GH_TOKEN.txt | docker login ghcr.io -u doridoridoriand --password-stdin;
 
+##########################
+# focal
+##########################
+docker tag `docker images ubuntu-lifeboat:focal-latest --format "{{.ID}}"` doridoridoriand/lifeboat-ubuntu:focal-latest;
+docker tag `docker images ubuntu-lifeboat:focal-latest --format "{{.ID}}"` doridoridoriand/lifeboat-ubuntu:focal-$unixtime;
+
+docker push doridoridoriand/lifeboat-ubuntu:focal-latest;
+docker push doridoridoriand/lifeboat-ubuntu:focal-$unixtime;
+
+docker tag `docker images ubuntu-lifeboat:focal-latest --format "{{.ID}}"` ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:focal-latest;
+docker tag `docker images ubuntu-lifeboat:focal-latest --format "{{.ID}}"` ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:focal-$unixtime;
+
+docker push ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:focal-latest;
+docker push ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:focal-$unixtime;
+
+##########################
+# jammy
+##########################
+docker tag `docker images ubuntu-lifeboat:jammy-latest --format "{{.ID}}"` doridoridoriand/lifeboat-ubuntu:jammy-latest;
+docker tag `docker images ubuntu-lifeboat:jammy-latest --format "{{.ID}}"` doridoridoriand/lifeboat-ubuntu:jammy-$unixtime;
+
+docker push doridoridoriand/lifeboat-ubuntu:jammy-latest;
+docker push doridoridoriand/lifeboat-ubuntu:jammy-$unixtime;
+
+docker tag `docker images ubuntu-lifeboat:jammy-latest --format "{{.ID}}"` ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:jammy-latest;
+docker tag `docker images ubuntu-lifeboat:jammy-latest --format "{{.ID}}"` ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:jammy-$unixtime;
+
+docker push ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:jammy-latest;
+docker push ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:jammy-$unixtime;
+
+##########################
+# lunar
+##########################
+docker tag `docker images ubuntu-lifeboat:lunar-latest --format "{{.ID}}"` doridoridoriand/lifeboat-ubuntu:latest;
+docker tag `docker images ubuntu-lifeboat:lunar-latest --format "{{.ID}}"` doridoridoriand/lifeboat-ubuntu:lunar-latest;
+docker tag `docker images ubuntu-lifeboat:lunar-latest --format "{{.ID}}"` doridoridoriand/lifeboat-ubuntu:lunar-$unixtime;
+
+docker push doridoridoriand/lifeboat-ubuntu:latest;
+docker push doridoridoriand/lifeboat-ubuntu:lunar-latest;
+docker push doridoridoriand/lifeboat-ubuntu:lunar-$unixtime;
+
+docker tag `docker images ubuntu-lifeboat:lunar-latest --format "{{.ID}}"` ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:latest;
+docker tag `docker images ubuntu-lifeboat:lunar-latest --format "{{.ID}}"` ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:lunar-latest;
+docker tag `docker images ubuntu-lifeboat:lunar-latest --format "{{.ID}}"` ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:lunar-$unixtime;
+
 docker push ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:latest;
-docker push ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:$unixtime;
+docker push ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:lunar-latest;
+docker push ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:lunar-$unixtime;
