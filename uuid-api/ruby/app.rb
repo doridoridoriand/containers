1ffebe4aa84fc5dfed9f711d095e2833cbe6d92e
Dorian (Takahiro Ishida) <i.am.eager.for.peace@gmail.com>
2024-01-20 14:45:17 +0900

    Correct for Rack lint error

--- uuid-api/ruby/app.rb
+++ uuid-api/ruby/app.rb
@@ -2,8 +2,12 @@ Cuba.use Rack::Session::Cookie, :secret => "idzbfxgtcurqwodxuy8Lywflaaqhp6bwbqbd
 
 Cuba.plugin Cuba::Safe
 
+# ref. https://github.com/rack/rack/blob/main/lib/rack/lint.rb#L655-L656
 Cuba.define do
   on root do
-    res.write JSON.dump({uuid: SecureRandom.uuid})
+    res.headers.keys.each do |key|
+      res.headers[key.downcase] = res.headers.delete(key)
+    end
+    res.json JSON.dump({uuid: SecureRandom.uuid})
   end
 end
