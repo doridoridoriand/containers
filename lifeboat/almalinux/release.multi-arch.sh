335a29c594db0183eb6a906ad539b8943881ae62
Dorian (Takahiro Ishida) <i.am.eager.for.peace@gmail.com>
2024-03-12 13:54:26 +0900

    Tweak scripts

--- lifeboat/almalinux/release.multi-arch.sh
+++ lifeboat/almalinux/release.multi-arch.sh
@@ -9,30 +9,33 @@ fi
 
 unixtime=`date +%s`;
 
- docker buildx rm lifeboat-builder
+docker buildx rm lifeboat-builder
 
- docker login;
+docker login;
 cat ~/GH_TOKEN.txt |  docker login ghcr.io -u doridoridoriand --password-stdin;
 docker buildx create --name lifeboat-builder
 docker buildx use lifeboat-builder
+
 ##########################
 # 8.8
 ##########################
 ######### Docker Hub #########
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-almalinux:8.8-$unixtime -f Dockerfile.8.8 .
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-almalinux:8.8-latest -f Dockerfile.8.8 .
+docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-almalinux:8.8-$unixtime -f Dockerfile.8.8 \
+                                                                                        --tag doridoridoriand/lifeboat-almalinux:8.8-latest -f Dockerfile.8.8
 ######### GitHub Packages #########
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:8.8-$unixtime -f Dockerfile.8.8 .
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:8.8-latest -f Dockerfile.8.8 .
+docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:8.8-$unixtime -f Dockerfile.8.8 \
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:8.8-latest -f Dockerfile.8.8
 
 ##########################
 # 9.2
 ##########################
 ######### Docker Hub #########
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-almalinux:9.2-$unixtime -f Dockerfile.9.2 .
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-almalinux:9.2-latest -f Dockerfile.9.2 .
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-almalinux:latest -f Dockerfile.9.2 .
+docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-almalinux:9.2-$unixtime -f Dockerfile.9.2 \
+                                                                                        --tag doridoridoriand/lifeboat-almalinux:9.2-latest -f Dockerfile.9.2 \
+                                                                                        --tag doridoridoriand/lifeboat-almalinux:latest -f Dockerfile.9.2
 ######### GitHub Packages #########
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:9.2-$unixtime -f Dockerfile.9.2 .
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:9.2-latest -f Dockerfile.9.2 .
- docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:latest -f Dockerfile.9.2 .
+docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:9.2-$unixtime -f Dockerfile.9.2 \
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:9.2-latest -f Dockerfile.9.2 \
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-almalinux:latest -f Dockerfile.9.2
+
+docker buildx rm lifeboat-builder
