320e928f8038944cdc4f636ded396e6675bded92
Dorian (Takahiro Ishida) <i.am.eager.for.peace@gmail.com>
2025-02-22 21:21:17 +0900

    Tweak release script.

--- lifeboat/ubuntu/release.multi-arch.sh
+++ lifeboat/ubuntu/release.multi-arch.sh
@@ -25,49 +25,36 @@ docker buildx use ${BUILDX_NAME}
 ####################################################
 # focal
 ####################################################
-######### Docker Hub #########
 docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-ubuntu:focal-$unixtime \
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:focal-$unixtime \
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:focal-latest \
                                                                                         --tag doridoridoriand/lifeboat-ubuntu:focal-latest -f Dockerfile.focal .
-
-######### GitHub Packages #########
-docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:focal-$unixtime \
-                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:focal-latest -f Dockerfile.focal .
-
 
 ####################################################
 # jammy
 ####################################################
-######### Docker Hub #########
 docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-ubuntu:jammy-$unixtime \
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:jammy-$unixtime \
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:jammy-latest \
                                                                                         --tag doridoridoriand/lifeboat-ubuntu:jammy-latest -f Dockerfile.jammy .
-
-######### GitHub Packages #########
-docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:jammy-$unixtime \
-                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:jammy-latest -f Dockerfile.jammy .
-
 
 ####################################################
 # noble
 ####################################################
-######### Docker Hub #########
 docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-ubuntu:noble-$unixtime \
-                                                                                        --tag doridoridoriand/lifeboat-ubuntu:noble-latest \
-                                                                                        --tag doridoridoriand/lifeboat-ubuntu:latest -f Dockerfile.noble .
-
-######### GitHub Packages #########
-docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:noble-$unixtime \
                                                                                         --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:noble-latest \
-                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:latest -f Dockerfile.noble .
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:noble-$unixtime \
+                                                                                        --tag doridoridoriand/lifeboat-ubuntu:latest \
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:latest \
+                                                                                        --tag doridoridoriand/lifeboat-ubuntu:noble-latest -f Dockerfile.noble .
 
 ####################################################
 # rolling
 ####################################################
-######### Docker Hub #########
 docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag doridoridoriand/lifeboat-ubuntu:rolling-$unixtime \
-                                                                                        --tag doridoridoriand/lifeboat-ubuntu:rolling-latest -f Dockerfile.rolling .
-
-######### GitHub Packages #########
-docker buildx build --push --platform=linux/arm64,linux/amd64,linux/s390x,linux/ppc64le --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:rolling-$unixtime \
+                                                                                        --tag doridoridoriand/lifeboat-ubuntu:rolling-latest \
+                                                                                        --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:rolling-$unixtime \
                                                                                         --tag ghcr.io/doridoridoriand/containers/lifeboat-ubuntu:rolling-latest -f Dockerfile.rolling .
+
 
 docker buildx rm ${BUILDX_NAME}
