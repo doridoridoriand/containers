2d85a2b6b4141fca7b2eba84314a901acfbcc3ff
Dorian (Takahiro Ishida) <i.am.eager.for.peace@gmail.com>
2025-04-16 15:13:11 +0900

    ひとまずTags一覧

---
+++ ollama-mirror/pull-push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export NAMESPACE=ollama
+export IMAGE=ollama
+tags=$(curl -s -H "Authorization: Bearer $(curl -s "https://auth.docker.io/token?scope=repository%3A${NAMESPACE}%2F${IMAGE}%3Apull&service=registry.docker.io" | jq -r '.token')" "https://registry-1.docker.io/v2/${NAMESPACE}/${IMAGE}/tags/list" | jq -r '.tags[]')
+echo  "Available tags: ${tags[@]}"
